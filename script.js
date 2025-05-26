5e39a0e2dde190f90a07a930c214c6ee77b7089d
Debalina Laha <32841776+Debalina99@users.noreply.github.com>
2025-05-26 23:27:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
-}
+  let s=s1.toLowerCase();
+  s2=s2.toLowerCase();
+  let found=false;
+  for(let i=0;i<s.length-1;i++){
+    for(let j=i+1;j<=s.length;j++){
+      let sub=s.slice(i,j);
+    // console.log(sub);
+      if(sub==s2){
+        found=true;
+        break;
+      }
+    }
+    if(found) return i;
+  }
 
+        return -1;
+    
+}
 // Please do not change the code below
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
